15f40c0713e5f9ef53893bbb0da959504f99da51
Standa Opichal <stanislav.opichal@rossum.ai>
2021-11-15 12:11:41 +0100

    fix(recursive): handle many=True serializers

--- drf_spectacular/contrib/rest_framework_recursive.py
+++ drf_spectacular/contrib/rest_framework_recursive.py
@@ -5,5 +5,4 @@ class RecursiveFieldExtension(OpenApiSerializerFieldExtension):
     target_class = "rest_framework_recursive.fields.RecursiveField"
 
     def map_serializer_field(self, auto_schema, direction):
-        component = auto_schema.resolve_serializer(self.target.proxied, direction)
-        return component.ref
+        return auto_schema._unwrap_list_serializer(self.target.proxied, direction)
